6f6bf34f89a39542ef817e4686b4606be0b4910e
Marc Rietz <mwrietz@gmail.com>
2022-12-03 09:34:48 -0600

    day 3 part 1

---
+++ day3/src/main.rs
@@ -0,0 +1,73 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let path = Path::new("input.txt"); 
+    let mut rucksacks = Vec::new();
+    read_file_to_vector(&path, &mut rucksacks);
+
+    let mut priority_sum = 0;
+    for rs in &rucksacks {
+        let rs_items: Vec<char> = rs.chars().collect();
+        let n = rs_items.len();
+        let rs_compartments: Vec<&[char]> =rs_items.chunks(n/2).collect();
+
+        priority_sum += priority(duplicate_item(rs_compartments[0], rs_compartments[1]))
+    }
+    println!("part 1");
+    println!("priority_sum: {}", priority_sum);
+}
+
+fn priority(item: char) -> i32 {
+    let p = item as i32;
+    if p>=97 && p<=122 {
+        return p-96;
+
+    }
+    if p>=65 && p<=90 {
+        return p-38;
+    }
+
+    0
+}
+
+fn duplicate_item(a1: &[char], a2: &[char]) -> char {
+    for c in a1 {
+        if a2.contains(c) {
+            return *c;
+        }
+    }
+    '_'
+}
+
+fn read_file_to_vector(file_path: &Path, vector: &mut Vec<String>) {
+    let file = File::open(file_path).expect("cannot open filepath");
+    let lines = io::BufReader::new(file).lines();
+
+    for line in lines {
+        if let Ok(ip) = line {
+            vector.push(ip);
+        }
+    }
+}
+
+/*
+fn read_file_to_vector(file_path: &Path, vector: &mut Vec<String>) {
+    if let Ok(lines) = read_lines(file_path) {
+        for line in lines {
+            if let Ok(ip) = line {
+                vector.push(ip);
+            }
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+*/
