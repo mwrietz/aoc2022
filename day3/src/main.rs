5ad45e3ca135055d9109293c954c14bf4fde61a3
Marc Rietz <mwrietz@gmail.com>
2022-12-03 17:37:47 -0600

    refactor day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,15 +1,23 @@
+/*
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+*/
 
 fn main() {
+    /*
     let path = Path::new("input.txt"); 
     let mut rucksacks = Vec::new();
     read_file_to_vector(&path, &mut rucksacks);
+    */
 
     // part 1
+    let rucksacks: Vec<&str> = include_str!("input.txt")
+        .lines()
+        .collect();
+
     let mut priority_sum = 0;
-    for rs in &rucksacks {
+    for rs in rucksacks {
         let rs_items: Vec<char> = rs.chars().collect();
         let n = rs_items.len();
         let rs_compartments: Vec<&[char]> = rs_items.chunks(n/2).collect();
@@ -20,7 +28,10 @@ fn main() {
     println!("priority_sum: {}", priority_sum);
 
     // part 2
-    let groups: Vec<&[String]> = rucksacks.chunks(3).collect();
+    let rucksacks: Vec<&str> = include_str!("input.txt")
+        .lines()
+        .collect();
+    let groups: Vec<&[&str]> = rucksacks.chunks(3).collect();
 
     let mut group_sum = 0;
     for group in groups {
@@ -57,6 +68,7 @@ fn duplicate_item(a1: &[char], a2: &[char]) -> char {
     '_'
 }
 
+/*
 fn read_file_to_vector(file_path: &Path, vector: &mut Vec<String>) {
     let file = File::open(file_path).expect("cannot open filepath");
     let lines = io::BufReader::new(file).lines();
@@ -67,3 +79,4 @@ fn read_file_to_vector(file_path: &Path, vector: &mut Vec<String>) {
         }
     }
 }
+*/
