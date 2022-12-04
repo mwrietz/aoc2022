62a1bc8b05c1103554e2deeb88a30a4334fdd9b8
Marc Rietz <mwrietz@gmail.com>
2022-12-04 17:41:21 -0600

    day 4 complete

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,15 +3,38 @@ fn main() {
 
     // part 1
     let mut n_fully_contained = 0;
+    let mut n_overlaps = 0;
     for line in lines {
         let assignments: Vec<&str> = line.split(",").collect(); 
         if contained(&assignments) {
             n_fully_contained += 1;
         }
+        if overlaps(&assignments) {
+            n_overlaps += 1;
+        }
     }
     println!("fully contained: {}", n_fully_contained);
+    println!("overlaps: {}", n_overlaps);
 
     // part 2
+}
+
+fn overlaps(a: &Vec<&str>) -> bool {
+    let (lb0, ub0) = bounds(a[0]);
+    let (lb1, ub1) = bounds(a[1]);
+    if lb0 >= lb1 && lb0 <= ub1 {
+        return true;
+    }
+    if ub0 >= lb1 && ub0 <= lb1 {
+        return true;
+    }
+    if lb1 >= lb0 && lb1 <= ub0 {
+        return true;
+    }
+    if ub1 >= lb0 && ub1 <= lb0 {
+        return true;
+    }
+    false
 }
 
 fn contained(a: &Vec<&str>) -> bool {
