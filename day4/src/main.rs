100a546b81ac59d0020f4f4fe63fd861347a0574
Marc Rietz <mwrietz@gmail.com>
2022-12-04 19:02:49 -0600

    refactor day 4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut n_fully_contained = 0;
     let mut n_overlaps = 0;
     for line in lines {
-        let assignments: Vec<&str> = line.split(",").collect(); 
+        let assignments: Vec<&str> = line.split(",").collect();
         if contained(&assignments) {
             n_fully_contained += 1;
         }
@@ -18,38 +18,38 @@ fn main() {
 }
 
 fn overlaps(a: &Vec<&str>) -> bool {
-    let (lb0, ub0) = bounds(a[0]);
-    let (lb1, ub1) = bounds(a[1]);
-    if lb0 >= lb1 && lb0 <= ub1 {
+    let (lb0, ub0, range0) = bounds(a[0]);
+    let (lb1, ub1, range1) = bounds(a[1]);
+
+    if range0.contains(&lb1) || range0.contains(&ub1) {
         return true;
     }
-    if ub0 >= lb1 && ub0 <= lb1 {
+    if range1.contains(&lb0) || range1.contains(&ub0) {
         return true;
     }
-    if lb1 >= lb0 && lb1 <= ub0 {
-        return true;
-    }
-    if ub1 >= lb0 && ub1 <= lb0 {
-        return true;
-    }
+
     false
 }
 
 fn contained(a: &Vec<&str>) -> bool {
-    let (lb0, ub0) = bounds(a[0]);
-    let (lb1, ub1) = bounds(a[1]);
-    if lb0 >= lb1 && ub0 <= ub1 {
+    let (lb0, ub0, range0) = bounds(a[0]);
+    let (lb1, ub1, range1) = bounds(a[1]);
+
+    if range0.contains(&lb1) && range0.contains(&ub1) {
         return true;
     }
-    if lb1 >= lb0 && ub1 <= ub0 {
+    if range1.contains(&lb0) && range1.contains(&ub0) {
         return true;
     }
+
     false
 }
 
-fn bounds(pair: &str) -> (i32, i32) {
+fn bounds(pair: &str) -> (i32, i32, std::ops::RangeInclusive<i32>) {
     let s: Vec<&str> = pair.split("-").collect();
     let lb: i32 = s[0].parse::<i32>().unwrap();
     let ub: i32 = s[1].parse::<i32>().unwrap();
-    (lb, ub)
+    let range = lb..=ub;
+
+    (lb, ub, range)
 }
