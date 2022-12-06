988428f0492b8afb513210a81cec25e426475b11
Marc Rietz <mwrietz@gmail.com>
2022-12-05 21:06:33 -0600

    delete target day 5

--- day5/target/debug/deps/day5-8cc5ecfa9f1afaf3.d
+++
@@ -1,6 +0,0 @@
-/Users/marc/Syncthing/active/computer/programming/rust/aoc/aoc2022/day5/target/debug/deps/day5-8cc5ecfa9f1afaf3.rmeta: src/main.rs src/./input.txt
-
-/Users/marc/Syncthing/active/computer/programming/rust/aoc/aoc2022/day5/target/debug/deps/day5-8cc5ecfa9f1afaf3.d: src/main.rs src/./input.txt
-
-src/main.rs:
-src/./input.txt:
--- day5/target/debug/deps/day5-f7e5e8740ca862f2.d
+++
@@ -1,6 +0,0 @@
-/Users/marc/Syncthing/active/computer/programming/rust/aoc/aoc2022/day5/target/debug/deps/day5-f7e5e8740ca862f2: src/main.rs src/./input.txt
-
-/Users/marc/Syncthing/active/computer/programming/rust/aoc/aoc2022/day5/target/debug/deps/day5-f7e5e8740ca862f2.d: src/main.rs src/./input.txt
-
-src/main.rs:
-src/./input.txt:
