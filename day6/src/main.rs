328e315011549d2060a7207800d52a58c73131d6
Marc Rietz <mwrietz@gmail.com>
2022-12-06 12:58:30 -0600

    day6 cleanup

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,24 +1,4 @@
 fn main() {
-    let buffer1: &str = include_str!("./input_test_1.txt");
-    let n1 = sop_marker_pos(buffer1);
-    println!("pos: {}", n1);
-
-    let buffer2: &str = include_str!("./input_test_2.txt");
-    let n2 = sop_marker_pos(buffer2);
-    println!("pos: {}", n2);
-
-    let buffer3: &str = include_str!("./input_test_3.txt");
-    let n3 = sop_marker_pos(buffer3);
-    println!("pos: {}", n3);
-
-    let buffer4: &str = include_str!("./input_test_4.txt");
-    let n4 = sop_marker_pos(buffer4);
-    println!("pos: {}", n4);
-
-    let buffer5: &str = include_str!("./input_test_5.txt");
-    let n5 = sop_marker_pos(buffer5);
-    println!("pos: {}", n5);
-
     let buffer: &str = include_str!("./input.txt");
     let n = sop_marker_pos(buffer);
     println!("pos: {}", n);
