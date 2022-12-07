3605e01abcebe2d35c7e4de584f644290f2185e9
Marc Rietz <mwrietz@gmail.com>
2022-12-06 19:41:26 -0600

    day6 refactor

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,27 +1,21 @@
 fn main() {
-    //part 1
     let buffer: &str = include_str!("./input.txt");
-    let n = sop_marker_pos(buffer);
-    println!("pos: {}", n);
-    //part 2
-    let part2buf: &str = include_str!("./input.txt");
-    let n2 = som_marker_pos(part2buf);
-    println!("pos: {}", n2);
+    println!("part1 pos: {}", marker_pos(buffer, 4));
+    println!("part2 pos: {}", marker_pos(buffer, 14));
 }
 
-// part 1
-fn sop_marker_pos(buf: &str) -> usize {
+fn marker_pos(buf: &str, marker_size: usize) -> usize {
     let v: Vec<char> = buf.chars().collect();
-    let mut pos: usize = 4;
+    let mut pos: usize = marker_size;
     loop {
-        let mut sop_marker: Vec<char> = Vec::new();
-        for i in 0..4 {
-            sop_marker.push(v[pos - 4 + i]);
+        let mut marker: Vec<char> = Vec::new();
+        for i in 0..marker_size {
+            marker.push(v[pos - marker_size + i]);
         }
-        let mut uniq = sop_marker.clone();
+        let mut uniq = marker.clone();
         uniq.sort();
         uniq.dedup();
-        if uniq.len() == sop_marker.len() {
+        if uniq.len() == marker.len() {
             break;
         }
         pos += 1;
@@ -29,25 +23,3 @@ fn sop_marker_pos(buf: &str) -> usize {
 
     pos
 }
-
-// part 2
-fn som_marker_pos(buf: &str) -> usize {
-    let v: Vec<char> = buf.chars().collect();
-    let mut pos: usize = 14;
-    loop {
-        let mut som_marker: Vec<char> = Vec::new();
-        for i in 0..14 {
-            som_marker.push(v[pos - 14 + i]);
-        }
-        let mut uniq = som_marker.clone();
-        uniq.sort();
-        uniq.dedup();
-        if uniq.len() == som_marker.len() {
-            break;
-        }
-        pos += 1;
-    }
-
-    pos
-}
-
